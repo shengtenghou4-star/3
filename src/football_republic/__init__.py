9d3eb12a3f6a76feafc07095c8be46b85d9b68fc
shengtenghou4-star <shengtenghou4@gmail.com>
2026-07-13 21:38:26 +0800

    feat: expose competition and roster APIs

--- src/football_republic/__init__.py
+++ src/football_republic/__init__.py
@@ -3,17 +3,33 @@
 from .campaign import Campaign, PresidentialPlan, Strategy, run_strategy
 from .domain import Club, NationalFootballSystem, Region
 from .engine import SimulationEngine
+from .football import (
+    ClubRoster,
+    DomesticLeague,
+    FootballWorld,
+    InternationalQualifiers,
+    MatchResult,
+    Player,
+    Standing,
+)
 from .programs import CoachEducationGrant, YouthMatchGrant
 from .scenario import build_2026_scenario
 
 __all__ = [
     "Campaign",
     "Club",
+    "ClubRoster",
     "CoachEducationGrant",
+    "DomesticLeague",
+    "FootballWorld",
+    "InternationalQualifiers",
+    "MatchResult",
     "NationalFootballSystem",
+    "Player",
     "PresidentialPlan",
     "Region",
     "SimulationEngine",
+    "Standing",
     "Strategy",
     "YouthMatchGrant",
     "build_2026_scenario",
