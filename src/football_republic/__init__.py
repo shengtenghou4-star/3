2de6608a8e6b9695c296ae65c7f92d989e45da0e
shengtenghou4-star <shengtenghou4@gmail.com>
2026-07-13 21:18:53 +0800

    feat: expose campaign API

--- src/football_republic/__init__.py
+++ src/football_republic/__init__.py
@@ -1,12 +1,21 @@
 """Football Republic public package API."""
 
-from .domain import NationalFootballSystem, Region
+from .campaign import Campaign, PresidentialPlan, Strategy, run_strategy
+from .domain import Club, NationalFootballSystem, Region
 from .engine import SimulationEngine
-from .programs import CoachEducationGrant
+from .programs import CoachEducationGrant, YouthMatchGrant
+from .scenario import build_2026_scenario
 
 __all__ = [
+    "Campaign",
+    "Club",
     "CoachEducationGrant",
     "NationalFootballSystem",
+    "PresidentialPlan",
     "Region",
     "SimulationEngine",
+    "Strategy",
+    "YouthMatchGrant",
+    "build_2026_scenario",
+    "run_strategy",
 ]
